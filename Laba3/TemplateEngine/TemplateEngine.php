634cad5346bbd00f63bcee6f140491ea34b355b8
Eugene851001 <jackz1630@mail.ru>
2020-03-20 18:32:34 +0300

    Template engine fixed

--- Laba3/TemplateEngine/TemplateEngine.php
+++ Laba3/TemplateEngine/TemplateEngine.php
@@ -4,7 +4,7 @@
 	$templates = array('{TIME}' => date('H:i:s'), 
 						'{IP}' => $_SERVER['REMOTE_ADDR'],
 						'{DATE}' => date('d m Y'));
-	$fileContent = file_get_contents('Compiler.php');
+	$fileContent = file_get_contents('Compiler.html');
 	foreach($templates as $key => $value) {
 		$fileContent = str_replace($key, $value, $fileContent);
 	}
