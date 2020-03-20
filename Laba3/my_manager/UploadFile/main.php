f735abc768f30670cacd25bcb3f97a3d590e7254
Eugene851001 <jackz1630@mail.ru>
2020-03-20 20:32:09 +0300

    remove some unnecessary files
    Was removed:

--- Laba3/my_manager/UploadFile/main.php
+++
@@ -1,8 +0,0 @@
-<form action="uploadFile.php" method="post" enctype="multipart/form-data">
-	<p>Изображения:
-		<input type="file" name="pictures[]" />
-		<input type="file" name="pictures[]" />
-		<input type="file" name="pictures[]" />
-		<input type="submit" value="Отправить" />
-	</p>
-</form>
