57051b23b2964665ff553022bfd72fcc1dc02d5d
Eugene851001 <jackz1630@mail.ru>
2020-03-27 19:21:30 +0300

    1

--- Laba3/Main.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" type="text/css" href="inputForm.css" />
-<?php echo $_SERVER['SCRIPT_NAME'];?>
-<form action="CreateCalendar.php" method="POST"  class =<?php echo 'incorrect-input;'?>>
-	Введите год<input class=<?php echo isset($_POST['incorrect']) ? 'incorrect-input' : '';?> 
-		type="text" 	name="year"/></br>
-	Введите курс<input class=<?php echo isset($_POST['incorrect']) ? 'incorrect-input' : '';?> type="text" name="course" /></br>
-	<input type="submit" value="Получить каледарь"/>
-</form>
